6cf217fe7caf15088f3f4759de047888d1acc8f3
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-08-01 02:44:45 +0300

    Corrections I

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -36,10 +36,11 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         with patch('client.GithubOrgClient.org',
                    new_callable=PropertyMock) as mock_org:
-            mock_org.return_value = {"repos_url": "http://some_url.com"}
+            mock_org.return_value = {'repos_url': 'http://some_url.com'}
             test_client = GithubOrgClient("some_org")
             self.assertEqual(test_client._public_repos_url,
-                             mock_org.return_value["repos_url"])
+                             "http://some_url.com")
+            mock_org.assert_called_once()
 
     @patch('client.get_json')
     def test_public_repos(self, mock_get_json):
